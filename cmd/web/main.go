55f0829ab8d613848b87f0d4f8b2a824de9cac03
sultan <sultan8512@gmail.com>
2021-06-02 12:50:14 +0530

    Backend devs basis compleated

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,17 +4,33 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/Encrypto07/Booking-App/pkg/config"
 	"github.com/Encrypto07/Booking-App/pkg/handlers"
 	"github.com/Encrypto07/Booking-App/pkg/render"
+	"github.com/alexedwards/scs/v2"
 )
 
 const portNumber = ":8080"
 
+var app config.AppConfig
+
+var session *scs.SessionManager
+
+//Main is the main application of a function
 func main() {
 
-	var app config.AppConfig
+	//Change this to true when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+	app.Session = session
+
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
 		log.Fatal("cannot create template cache")
