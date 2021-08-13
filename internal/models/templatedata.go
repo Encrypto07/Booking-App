8dbbbd28aa867c49b292be920ab15d3c61b6defd
sultan <sultan8512@gmail.com>
2021-08-13 07:58:06 +0530

    added authentication ie login and logout page and also apply authentaction

--- internal/models/templatedata.go
+++ internal/models/templatedata.go
@@ -4,13 +4,14 @@ import "github.com/Encrypto07/Booking-App/internal/forms"
 
 //TemplateData holds dats sent frm handlers to templates
 type TemplateData struct {
-	StringMap map[string]string
-	IntMap    map[string]int
-	FloatMap  map[string]float32
-	Data      map[string]interface{}
-	CSRFToken string
-	Flash     string
-	Warning   string
-	Error     string
-	Form      *forms.Form
+	StringMap       map[string]string
+	IntMap          map[string]int
+	FloatMap        map[string]float32
+	Data            map[string]interface{}
+	CSRFToken       string
+	Flash           string
+	Warning         string
+	Error           string
+	Form            *forms.Form
+	IsAuthenticated int
 }
